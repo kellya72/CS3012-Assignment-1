eb5d9f9718b37ff7163a278091f10515ea46f937
kellya72 <32570386+kellya72@users.noreply.github.com>
2017-10-17 20:36:55 +0100

    Add files via upload
    Updated code

--- LCA.java
+++ LCA.java
@@ -6,51 +6,52 @@ import java.util.List;
 public class LCA
 {
  
-    Node root;
-    private List<Integer> path1 = new ArrayList<>();
+    Node r;
+    private List<Integer> path1 = new ArrayList<>();  
     private List<Integer> path2 = new ArrayList<>();
  
-    int findLCA(int n1, int n2) {
+    int findLCA(int no1, int no2) {
         path1.clear();
         path2.clear();
-        return findLCAInternal(root, n1, n2);
+        return findLCA(r, no1, no2);
     }
  
-    private int findLCAInternal(Node root, int n1, int n2) {
- 
-        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
+    private int findLCA(Node r, int no1, int no2) {
+    	int number = 0;
+        if (!findPath(r, no1, path1) || !findPath(r, no2, path2)) {
             System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing");
             System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing");
             return -1;
         }
  
-        int i;
-        for (i = 0; i < path1.size() && i < path2.size(); i++) {
+        int index;
+        for (index = 0; index < path1.size() && index < path2.size(); index++) {
           //  System.out.println(path1.get(i) + " " + path2.get(i));
-            if (!path1.get(i).equals(path2.get(i)))
-                break;
+            if (!path1.get(index).equals(path2.get(index)))
+                number=index-1;
+            	break;
         }
  
-        return path1.get(i-1);
+        return path1.get(number);
     }
  
-    private boolean findPath(Node root, int n, List<Integer> path)
+    private boolean findPath(Node r, int n, List<Integer> path)
     {
-        if (root == null) {
+        if (r == null) {
             return false;
         }
  
-        path.add(root.data);
+        path.add(r.data);
  
-        if (root.data == n) {
+        if (r.data == n) {
             return true;
         }
  
-        if (root.left != null && findPath(root.left, n, path)) {
+        if (r.left != null && findPath(r.left, n, path)) {
             return true;
         }
  
-        if (root.right != null && findPath(root.right, n, path)) {
+        if (r.right != null && findPath(r.right, n, path)) {
             return true;
         }
  
@@ -62,13 +63,13 @@ public class LCA
     public static void main(String[] args)
     {
         LCA tree = new LCA();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-        tree.root.right.left = new Node(6);
-        tree.root.right.right = new Node(7);
+        tree.r = new Node(1);
+        tree.r.left = new Node(2);
+        tree.r.right = new Node(3);
+        tree.r.left.left = new Node(4);
+        tree.r.left.right = new Node(5);
+        tree.r.right.left = new Node(6);
+        tree.r.right.right = new Node(7);
  
         System.out.println("LCA(4, 5): " + tree.findLCA(4,5));
         System.out.println("LCA(4, 6): " + tree.findLCA(4,6));
