21db457339f8cfc2aa28df02906720f95def489c
kellya72 <32570386+kellya72@users.noreply.github.com>
2017-10-27 14:49:39 +0100

    Add files via upload
    Node classes modified to include a count value and a colour

--- Node.java
+++ Node.java
@@ -1,9 +1,24 @@
 class Node {
     int data;
     Node left, right;
- 
+    String colour;
+    int count;
     Node(int value) {
         data = value;
         left = right = null;
+        colour= "white";
+        count=0;
+    }
+    private void setColour(String c){
+    	colour=c;
+    }
+    private String getColour(){
+    	return colour;
+    }
+    private void setCount(int c){
+    	count=c;
+    }
+    private int getCount(){
+    	return count;
     }
 }
