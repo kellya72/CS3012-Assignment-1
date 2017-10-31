375481d17935b40baa35037e69ccf8dd6db04fdd
kellya72 <32570386+kellya72@users.noreply.github.com>
2017-10-31 14:57:21 +0000

    Add files via upload
    Further work on initialising a node.

--- Node.java
+++ Node.java
@@ -1,24 +1,19 @@
+import java.util.ArrayList;
+
+import dummy.node2;
+
+
 class Node {
     int data;
-    Node left, right;
-    String colour;
-    int count;
-    Node(int value) {
-        data = value;
-        left = right = null;
-        colour= "white";
-        count=0;
-    }
-    private void setColour(String c){
-    	colour=c;
-    }
-    private String getColour(){
-    	return colour;
-    }
-    private void setCount(int c){
-    	count=c;
-    }
-    private int getCount(){
-    	return count;
+    public ArrayList<Node> edges;
+    public int key;
+    public int in;
+    public int out;
+    Node(int data, int key) {
+        this.data = data;
+        this.key = key;
+        edges = new ArrayList<Node>();
+        in = 0;
+        out = edges.size();
     }
 }
