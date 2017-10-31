3eea749736d4a3413382dea4a92a48c94abccb00
kellya72 <32570386+kellya72@users.noreply.github.com>
2017-10-31 15:57:05 +0000

    Add files via upload
    Decided to alter the findLCA class parameters as the current function was not compatable for a dag

--- LCA.java
+++ LCA.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 
 
+
  
 
 
 public class LCA
 {
  
-   /* Node r;
+    Node r;
     private List<Integer> path1 = new ArrayList<>();  
     private List<Integer> path2 = new ArrayList<>();
  
@@ -21,55 +22,15 @@ public class LCA
         return findLCA(r, no1, no2);
     }
  
-    private int findLCA(Node r, int no1, int no2) {
-        if (!findPath(r, no1, path1) || !findPath(r, no2, path2)) {
-            if(path1.size() > 0) {
-            	System.out.println("n1 is present"); 
-            }
-            if(!(path1.size()>0)){
-            	System.out.println("n1 is missing");
-            }
-            if(path2.size() > 0) {
-            	System.out.println("n2 is present"); 
-            }
-            if(!(path2.size()>0)){
-            	System.out.println("n2 is missing");
-            }
-            return -1;
-        }
+    private int findLCA(ArrayList<Node> nodeList, Node a, Node b) {
+        
  
-        int index;
-        for (index = 0; index < path1.size() && index < path2.size(); index++) {
-            if (!path1.get(index).equals(path2.get(index)))
-            	break;
-        }
- 
-        return path1.get(index-1);
     }
  
     private boolean findPath(Node r, int n, List<Integer> path)
     {
-        if (r == null) {
-            return false;
-        }
- 
-        path.add(r.data);
- 
-        if (r.data == n) {
-            return true;
-        }
- 
-        if (r.left != null && findPath(r.left, n, path)) {
-            return true;
-        }
- 
-        if (r.right != null && findPath(r.right, n, path)) {
-            return true;
-        }
- 
-        path.remove(path.size()-1);
-        return false;
-    }*/
+       
+    }
     private static boolean cycle(ArrayList<Node> arrList, Node temp, ArrayList<Node> visited, ArrayList<Node> stack, boolean cycle)
 	{
 		visited.add(temp);
