5fc039123c44d207d8d6406eb60c1df2171b0713
kellya72 <32570386+kellya72@users.noreply.github.com>
2017-11-01 17:06:57 +0000

    Add files via upload
    Testing the findLCA function when the parameters are null.

--- LCATest.java
+++ LCATest.java
@@ -1,12 +1,14 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class LCATest {
 
 	@Test
 	public void testFindLCA() {
-		LCA testTree = new LCA();
+		/*LCA testTree = new LCA();
         assertSame("Checking for correct lowest common ancestor when tree is empty", -1, testTree.findLCA(3,2));		
         testTree.r = new Node(1);
         assertSame("Checking for correct lowest common ancestor when tree contains one node", 1, testTree.findLCA(1,1));	
@@ -29,24 +31,37 @@ public class LCATest {
         assertSame("Checking for correct lowest common ancestor",-1, testTree.findLCA(8,5));
         assertSame("Checking for correct lowest common ancestor",-1, testTree.findLCA(8,7));
         assertSame("Checking for correct lowest common ancestor",-1, testTree.findLCA(4,9));
-        
+       */ 
+		Node a = null;
+		Node b= null;
+		ArrayList<Node> list = new ArrayList<Node>();
+		assertEquals("When nodes and list are null", null, LCA.findLCA(list,a ,b));
+	    a= new Node("a",1);
+	    assertEquals("When one node and list are null", null, LCA.findLCA(list,a ,b));
+	    b= new Node("b",2);
+	    assertEquals("When the list is null", null, LCA.findLCA(list,a ,b));
 	}
-	
+	@Test
 	public void testCompare(){
 		
 	}
+	@Test
 	public void testCycle(){
 		
 	}
+	@Test
 	public void testIsAcyclical(){
 		
 	}
+	@Test
 	public void testCompareAncestors(){
 		
 	}
+	@Test
 	public void testCheck(){
 		
 	}
+	@Test
 	public void testListToString(){
 		
 	}
